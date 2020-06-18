3529299b95b313083094e7e5bd1eb15a4c8e35b3
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-06-18 21:00:55 +0700

    Last commit

--- application/controllers/Channels.php
+++ application/controllers/Channels.php
@@ -2,6 +2,16 @@
 
 class Main extends CI_Controller {
 
+	public function get() {
+		$categories = $this->db->query("SELECT * FROM `channels` GROUP BY `category`");
+		for ($i=0; $i<sizeof($categories); $i++) {
+			$category = $categories[$i]['category'];
+			$channels = $this->db->query("SELECT * FROM `channels` WHERE `category`='" . $category . "'")->result_array();
+			$category['channels'] = $channels;
+		}
+		echo json_encode($categories);
+	}
+
 	public function add() {
 		$title = $this->input->post('title');
 		$logo = $this->input->post('logo');
--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,14 +1,4 @@
 <?php
 
 class Main extends CI_Controller {
-
-	public function get_channels() {
-		$categories = $this->db->query("SELECT * FROM `channels` GROUP BY `category`");
-		for ($i=0; $i<sizeof($categories); $i++) {
-			$category = $categories[$i]['category'];
-			$channels = $this->db->query("SELECT * FROM `channels` WHERE `category`='" . $category . "'")->result_array();
-			$category['channels'] = $channels;
-		}
-		echo json_encode($categories);
-	}
 }
